4ab7ce4425cb4cb8556be08a9c47060d610ef1d5
reynaldaniel <32169517+reynaldaniel@users.noreply.github.com>
2018-03-28 12:06:53 +0700

    reverse

--- fibbonaci/jurnal/src/jurnal/Jurnal.java
+++ fibbonaci/jurnal/src/jurnal/Jurnal.java
@@ -13,26 +13,45 @@ import java.util.Scanner;
  */
 public class Jurnal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner rey = new Scanner(System.in);
-        System.out.print("Masukkan jumlah angka : ");
-        int in = rey.nextInt();
+    Scanner rey;
+        int a1, b1, c1, a2, b2, c2, limit, i;
         
-        int fib[] = new int[in];
-        
-        fib[0] = 0;
-        fib[1] = 1;
-        
-        for (int i = 2; i < in; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
-        }
-        for (int i = 0; i < in; i++) {
-            System.out.print(fib[i] + " ");
+    public Jurnal(){
+        rey = new Scanner (System.in);
+        a1 = c1 = 1;
+        b1 = a2 = b2 = c2 = limit = i = 0;
+    }
+    public void input(){
+        try {
+            System.out.printf("Enter limit: ");
+            limit = rey.nextInt();
+            rey.close();
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.err.println("Error Occurred!");
+            System.exit(0);
         }
     }
+    public void compute() {
+        for (; i < limit; i++) {
+            a1 = b1;
+            b1 = c1;
+            c1 = a1 + b1;
+            a2 = c1;
+            c2 = b1;
+        }
+        System.out.println("Series in Reverse Order:");
+        for (i = limit - 1; i >= 0; i--) {
+            b2 = a2;
+            a2 = c2;
+            c2 = b2 - a2;
+            System.out.print(c2+" ");
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jurnal jr =  new Jurnal();
+        jr.input();
+        jr.compute();
     
+    }
 }
